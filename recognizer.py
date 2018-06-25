b5f58747cddf472835bc588513645951724338f4
Katharina Lichtner <Katharina.Lichtner@stud.uni-regensburg.de>
2018-06-25 12:56:17 -0400

    fertiges UI

--- recognizer.py
+++ recognizer.py
@@ -6,7 +6,7 @@ import wiimote
 import time
 import sys
 import numpy as np
-from PyQt5 import uic, QtWidgets, QtCore, QtGui
+from PyQt5 import uic, QtWidgets, QtCore, QtGui, Qt
 from transform import Transform
 
 
@@ -20,20 +20,64 @@ class Window(QtWidgets.QWidget):
         self.coordinates = []
         self.initUI()
 
-
+# making Boxlayouts within a boxlayout
+# https://stackoverflow.com/questions/49127653/align-every-widget-of-a-qhboxlayout-to-the-top-in-pyqt
+# last accessed 25.06.2018
+# line edit hint text:
+# http://doc.qt.io/qt-5/qtwidgets-widgets-lineedits-example.html
+# last access 25.06.2018
     def initUI(self):
 
+        self.layout = QtWidgets.QVBoxLayout(self)
         self.pushButton = QtWidgets.QPushButton("Add Symbol")
-        self.layout = QtWidgets.QHBoxLayout(self)
 
-       # self.pushButton.move(50, 50)
-        self.pushButton.setFixedWidth(50)
-        self.pushButton.setFixedHeight(50)
-        self.pushButton.pos().setY(100)
-       # self.pushButton.setStyleSheet("align: right")
-        self.layout.addWidget(self.pushButton)
+
+        self.dropdown = QtWidgets.QComboBox(self)
+        self.exampleText = QtWidgets.QLabel(self)
+        self.exampleText.setText("Add example of type")
+        self.addExamplelayout = QtWidgets.QHBoxLayout()
+        self.addExamplelayout.addWidget(self.exampleText)
+        self.dropdown.addItem("Snake")
+        self.dropdown.addItem("Rectangle")
+        self.dropdown.addItem("Circle")
+        self.addExamplelayout.addWidget(self.dropdown)
+        self.addExamplelayout.addWidget(self.pushButton)
+
+
+        self.addGestureText = QtWidgets.QLabel(self)
+        self.addGestureText.setText("Add Gesture of type")
+        self.addGestureLayout = QtWidgets.QHBoxLayout()
+        self.addGestureLayout.addWidget(self.addGestureText)
+        self.gestureInput = QtWidgets.QLineEdit()
+        self.gestureInput.setPlaceholderText("Add symbol/gesture of type")
+        self.addGestureLayout.addWidget(self.gestureInput)
+        self.addGestureButton = QtWidgets.QPushButton("Add gesture/symbol")
+        self.addGestureLayout.addWidget(self.addGestureButton)
+
+
+
+
+        self.resultText = QtWidgets.QLabel(self)
+        self.resultText.setText("Result: ")
+        self.resultOutputText = QtWidgets.QLabel(self)
+        self.resultLayout = QtWidgets.QHBoxLayout()
+        self.resultLayout.addWidget(self.resultText)
+        self.resultLayout.addWidget(self.resultOutputText)
+
+
+        self.addExamplelayout.setAlignment(QtCore.Qt.AlignBottom)
+        self.addGestureLayout.setAlignment(QtCore.Qt.AlignBottom)
+        self.resultLayout.setAlignment(QtCore.Qt.AlignBottom)
+
+        self.layout.addLayout(self.addExamplelayout)
+
+        self.layout.addLayout(self.addGestureLayout)
+        self.layout.addLayout(self.resultLayout)
+        self.layout.setAlignment(QtCore.Qt.AlignBottom)
+
+
         self.setGeometry(0, 0, 1280, 720)
-        self.setWindowTitle('1$ Recognizer')
+        self.setWindowTitle('$1 Recognizer')
         QtGui.QCursor.setPos(self.mapToGlobal(
             QtCore.QPoint(self.start_pos[0], self.start_pos[0])))
         self.show()
