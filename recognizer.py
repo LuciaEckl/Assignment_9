7e38d6535b469bf78f47cc74c9d391f82dced629
Katharina Lichtner <Katharina.Lichtner@stud.uni-regensburg.de>
2018-06-26 13:59:44 -0400

    Rotation neu

--- recognizer.py
+++ recognizer.py
@@ -19,12 +19,15 @@ class Window(QtWidgets.QWidget):
         self.mouseMovePos = (0, 0)
         self.draw = False
         self.coordinates = []
-        self.pointNumber = 255
+        self.pointNumber = 64
         self.pushButton = QtWidgets.QPushButton("Add Symbol")
         self.addGestureButton = QtWidgets.QPushButton("Add gesture/symbol")
         self.X = 0
         self.Y = 0
         self.resampledPoints = []
+        self.square_size = 100
+        self.drawNewPoints = False
+        self.newPointsToDraw = []
         self.initUI()
 
 # making Boxlayouts within a boxlayout
@@ -93,15 +96,20 @@ class Window(QtWidgets.QWidget):
 
 
     def mousePressEvent(self, ev):
-        print("clicked")
+        #print("clicked")
         self.draw = True
 
     def mouseReleaseEvent(self, ev):
-        print("release")
+        #print("release")
         self.draw = False
-        print("relese", self.coordinates)
+        #print("relese", self.coordinates)
         self.resampledPoints = self.resample(self.coordinates, self.pointNumber)
-        self.rotateToZero(self.resampledPoints)
+        angle = self.indicativeAngle(self.resampledPoints)
+        self.rotatedPoints = self.rotateBy(self.resampledPoints, -angle)
+        #print(self.newPointsToDraw)
+        self.drawNewPoints = True
+
+
 
 
 
@@ -118,15 +126,23 @@ class Window(QtWidgets.QWidget):
             qp.begin(self)
             if self.draw is True:
                 self.drawtarget(qp)
-
+            if self.drawNewPoints is True:
+                print("Draw is True")
+                self.drawNewTargetPoints(qp)
+                self.drawNewPoints = False
 
             qp.end()
+
+    def drawNewTargetPoints(self, qp):
+        qp.setBrush(QtGui.QColor(0, 200, 0))
+        for i in range(len(self.newPointsToDraw) - 1):
+            qp.drawLine(self.newPointsToDraw[i][0], self.newPointsToDraw[i][1], self.newPointsToDraw[i + 1][0], self.newPointsToDraw[i +1][1])
+
 
 
     def drawtarget(self, qp):
         qp.setBrush(QtGui.QColor(0, 200, 0))
         for i in range(len(self.coordinates) - 1):
-            print(self.coordinates)
             qp.drawLine(self.coordinates[i][0], self.coordinates[i][1], self.coordinates[i + 1][0], self.coordinates[i +1][1])
 
     def resample(self, points, numberOfPoints):
@@ -135,11 +151,11 @@ class Window(QtWidgets.QWidget):
         newpoints = [points[0]]
         newpoints.append(points[0])
         for i in range(1, len(points)):
-            print("punkte", points[i-1], points[i])
+           # print("punkte", points[i-1], points[i])
             distance = self.distance(points[i - 1], points[i])
-            print("distance", distance)
+           # print("distance", distance)
             if distance + d >= intervalLength:
-                print("im if")
+            #    print("im if")
                 self.q = [0.0, 0.0]
                 self.q[0] = points[i-1][0] + float((intervalLength - d) / distance) * (points[i][0] - points[i-1][0])
                 self.q[1] = points[i-1][1] + float((intervalLength - d) / distance) * (points[i][1] - points[i-1][1])
@@ -152,7 +168,7 @@ class Window(QtWidgets.QWidget):
         if len(newpoints) == numberOfPoints -1:
             newpoints.append(points[0])
 
-        print(newpoints)
+        print(len(newpoints), self.pointNumber)
         return newpoints
 
 
@@ -167,23 +183,50 @@ class Window(QtWidgets.QWidget):
         return d
 
     def distance(self, p1, p2):
-        print("fehlersuche", p1, p2)
+      #  print("fehlersuche", p1, p2)
         dx = p2[0] - p1[0]
         dy = p2[1] - p1[1]
         return float(np.sqrt(dx * dx + dy * dy))
 
     def indicativeAngle(self, points):
-        centroid = np.mean(points,0)
+        centroid = self.centroid(points)
         rotation_angle = np.arctan2(centroid[1] - points[0][1], centroid[0]-points[0][0])
         return rotation_angle
 
-    def rotateToZero(self, points):
-        rotation_angle = self.indicativeAngle(points)
-        newPoints = self.rotate2D(points, 0, -rotation_angle)
+    def rotateBy(self, points, radians):
+        centroid = self.centroid(points)
+        cos = np.cos(radians)
+        sin = np.sin(radians)
+        newPoints = np.zeros((1,2))
+        for i in range(len(points)):
+            q = np.array([0.0, 0.0])
+            q[0] = (points[i][0] - centroid[0]) * cos - (points[i][1] - centroid[1]) * sin + centroid[0]
+            q[1] = (points[i][0] - centroid[0]) * sin + (points[i][1] - centroid[1]) * cos + centroid[1]
+            newPoints = np.append(newPoints, [q], 0)
+
+        print("newpoints", newPoints)
         return newPoints
 
     def rotate2D(self, points, count, ang=np.pi/4):
         return np.dot(np.array(points)-count, np.array([[np.cos(ang), np.sin(ang)], [-np.sin(ang), np.cos(ang)]])) + count
+
+
+    def scaleToSquare(self, points):
+        maxX, maxY = np.max(points, 0)
+        minX, minY = np.min(points, 0)
+        b_width = maxX - minX
+        b_height = maxY - minY
+        new_points = np.zeros((1,2))
+        for point in points:
+            q = np.array([0.0, 0.0])
+            q[0] = point[0] * (self.square_size / b_width)
+            q[1] = point[1] * (self.square_size / b_height)
+            new_points = np.append(new_points, [q], 0)
+        return new_points[1:]
+
+    def centroid(self, points):
+        centroid = np.mean(points,0)
+        return centroid
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
